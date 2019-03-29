39b4f6b2cd2d09084b25d717e5cc976fb1427437
george <gkephart@cnm.edu>
2019-03-28 20:52:33 -0600

    since react combines html with javascript it is generally best practice to make a lot of small components instead of massive components

--- components-and-props/src/index.js
+++ components-and-props/src/index.js
@@ -17,7 +17,45 @@ const App = () => {
 						<span className="date">Today at 6:35pm</span>
 					</div>
 					<div className="text">
-						Can you pimp this powerpoint, need more geometry patterns i love it, but can you invert all colors? or just do what you think. I trust you, or that's great, but we need to add this 2000 line essay yet just do what you think.
+						Can you pimp this powerpoint, need more geometry patterns i love it, but can you invert all colors? or
+						just do what you think. I trust you, or that's great, but we need to add this 2000 line essay yet just
+						do what you think.
+					</div>
+				</div>
+			</div>
+			<div className="comment">
+				<a href="/" className="avatar">
+					<img alt="avatar" src={faker.image.avatar()}/>
+				</a>
+				<div className="content">
+					<a href="http://" className="author">
+						Sam Jones
+					</a>
+					<div className="metadata">
+						<span className="date">Today at 6:35pm</span>
+					</div>
+					<div className="text">
+						Can you pimp this powerpoint, need more geometry patterns i love it, but can you invert all colors? or
+						just do what you think. I trust you, or that's great, but we need to add this 2000 line essay yet just
+						do what you think.
+					</div>
+				</div>
+			</div>
+			<div className="comment">
+				<a href="/" className="avatar">
+					<img alt="avatar" src={faker.image.avatar()}/>
+				</a>
+				<div className="content">
+					<a href="http://" className="author">
+						Sam Jones
+					</a>
+					<div className="metadata">
+						<span className="date">Today at 6:35pm</span>
+					</div>
+					<div className="text">
+						Can you pimp this powerpoint, need more geometry patterns i love it, but can you invert all colors? or
+						just do what you think. I trust you, or that's great, but we need to add this 2000 line essay yet just
+						do what you think.
 					</div>
 				</div>
 			</div>
