f416cdc950c5cd780f57991ff3f7a31e250a551f
george <gkephart@cnm.edu>
2019-03-28 18:52:00 -0600

    Yay variables

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -4,14 +4,16 @@ import ReactDOM from 'react-dom'
 
 //create a react component
 const App = () => {
-  return(
-	  <div><label className="label" for="name">Enter Name</label>
-		  <input type="text"/>
-		  <button style={{backgroundColor:'blue', color: 'white;'}}>click me please</button>
-	  </div>   
-  );
+
+	const getButtonText = () => "click me pretty please";
+	return (
+		<div><label className="label" for="name">Enter Name</label>
+			<input type="text"/>
+			<button style={{backgroundColor: 'blue', color: 'white;'}}>{getButtonText()}</button>
+		</div>
+	);
 };
 
 // display the component on the domain
-ReactDOM.render(<App />, document.querySelector('#root',));
+ReactDOM.render(<App/>, document.querySelector('#root',));
 
