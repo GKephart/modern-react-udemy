dc5a1b022e103d23371b359d3614c07ef9671043
george <gkephart@cnm.edu>
2019-03-28 18:40:38 -0600

    completed inline-css with React

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -4,8 +4,14 @@ import ReactDOM from 'react-dom'
 
 //create a react component
 const App = () => {
-  return <div> Hello World</div>;
-}
+  return(
+	  <div><label class="label" for="name">Enter Name</label>
+		  <input type="text"/>
+		  <button style={{backgroundColor:'blue', color: 'white;'}}>click me please</button>
+	  </div>   
+  );
+};
 
 // display the component on the domain
 ReactDOM.render(<App />, document.querySelector('#root',));
+
