a005af4d04cbe0e25533f2f38b75ab33ba0c4997
george <gkephart@cnm.edu>
2019-03-28 19:09:19 -0600

    variables in jsx

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -5,15 +5,28 @@ import ReactDOM from 'react-dom'
 //create a react component
 const App = () => {
 
+	//this works
+	const buttonText = "click me";
+
+	//array duh.
+	const buttonTextArray = ["hi", "there"];
+
+	const buttonTextObject = {text: "click me"};
+
+	//es6 accessor function
 	const getButtonText = () => "click me pretty please";
-	return (
-		<div><label className="label" for="name">Enter Name</label>
-			<input type="text"/>
-			<button style={{backgroundColor: 'blue', color: 'white;'}}>{getButtonText()}</button>
-		</div>
-	);
+  return(
+	  <div><label className="label" for="name">Enter Name</label>
+		  <input type="text"/>
+		  <button style={{backgroundColor:'blue', color: 'white'}}>{buttonText}</button>
+		  <button style={{backgroundColor:'white', color: 'blue'}}>{buttonTextArray}</button>
+		  <button style={{backgroundColor:'red', color: 'light-gray'}}>{getButtonText()}</button>
+		  {/*how to use objects IE use the object accessor*/}
+		  <button style={{backgroundColor:'blue', color: 'white'}}>{buttonTextObject.text}</button>
+</div>
+  );
 };
 
 // display the component on the domain
-ReactDOM.render(<App/>, document.querySelector('#root',));
+ReactDOM.render(<App />, document.querySelector('#root',));
 
