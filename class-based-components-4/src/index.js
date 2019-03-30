9098d2dd7a64d4666aa40bc30a3bca5fb580057d
george <gkephart@cnm.edu>
2019-03-30 01:34:13 -0600

    57 finished section 5 with conditional loading now headed into section 6

--- class-based-components-4/src/index.js
+++ class-based-components-4/src/index.js
@@ -11,34 +11,33 @@ class App extends React.Component {
 
 		// sets up state for the App object
 		this.state = {lat: null};
+
+		window.navigator.geolocation.getCurrentPosition(
+			position => {
+
+				//in order to update state the setState mutator must be called
+				this.setState({lat: position.coords.latitude});
+
+				//never directly set state after init TLDR this.state.foo = "Y U NO use this.setState() mutator"
+				console.log(position);
+			} ,
+			(error) => {
+				this.setState({lat: - 35.11, error: error.message});
+
+			}
+		);
 	}
 
 
 	//react demands we have a render method if we extend React.Component cough cough contract.
 	render() {
 		let foo = "poo";
-
-			window.navigator.geolocation.getCurrentPosition(
-				position => {
-
-					//in order to update state the setState mutator must be called
-					this.setState({lat: position.coords.latitude});
-
-					//never directly set state after init TLDR this.state.foo = "Y U NO use this.setState() mutator"
-					console.log(position);
-				} ,
-				(error) => {
-					this.setState({lat: - 35.11, error: error.message});
-
-				}
-			);
-
-		return (
-			<div>
-				<h1>mason is a {foo} {this.state.lat} bear</h1>
-				mason {foo} bear caused a {this.state.message}
-			</div>
-		)
+		if(!this.state.error && this.state.lat) {
+			return <div>Mason {foo} bear is eating a jar of honey @ {this.state.lat} </div>
+		} else if (!this.state.error && !this.state.lat !== -35){
+			return <div>Mason {foo} Bear caused {this.state.error}</div>
+		}
+		return <h1>Y U NO let me load</h1>
 	}
 }
 
