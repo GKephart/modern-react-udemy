4b5f9ea0359d50ef78902a5c942392781a152511
george <gkephart@cnm.edu>
2019-03-29 21:04:47 -0600

    51 much prefer functional components

--- class-based-components-4/src/index.js
+++ class-based-components-4/src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 
-let foo = "poo";
-let getCurrentLocation = () => (
-	window.navigator.geolocation.getCurrentPosition(
-		position => position,
-		error => console.log(error)
-	));
 
-const App = () => (
-	<div>
-		<h1>mason is a {foo} {getCurrentLocation()} bear</h1>
-	</div>
-);
+class App extends React.Component {
+
+
+	render() {
+		let foo = "poo";
+
+		let getCurrentLocation = () => (
+			window.navigator.geolocation.getCurrentPosition(
+				position => console.log(position),
+				error => console.log(error)
+			));
+
+		return (
+			<div>
+				<h1>mason is a {foo} {getCurrentLocation()} bear</h1>
+			</div>
+		)
+	}
+}
 
 ReactDOM.render(<App/>, document.querySelector('#root',));
