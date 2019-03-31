f14595e2b9906871ba2316c862a3e5d6959be98b
george <gkephart@cnm.edu>
2019-03-31 13:46:24 -0600

    64 conditionals using ternary operators interacting with JSX.

--- class-based-components-4/src/SeasonDisplay.js
+++ class-based-components-4/src/SeasonDisplay.js
@@ -1,20 +1,14 @@
 import React from 'react';
 
 const getSeason = (lat, month) => {
-	if( month > 2 && month < 9) {
-		return lat > 0 ? 'summer' : 'winter';
-	} else {}
-	return lat > 0 ? 'winter' : 'summer';
-
+	return month > 2 && month < 9 ? lat > 0 ? 'summer' : 'winter' : lat > 0 ? 'winter' : 'summer';
 };
-
-
 const SeasonDisplay = (props) => {
 	const season = getSeason(props.lat, new Date().getMonth());
+	//  happens in the {season}
 	return (
-		<h2>Mason's super soft birthday party happens in the {season}</h2>
+		<h2>{season === 'winter' ? "Mason's super soft party will be chilly" : "Mason's super soft party will be hawt"}</h2>
 	);
 };
-
-	export default SeasonDisplay;
+export default SeasonDisplay;
 
