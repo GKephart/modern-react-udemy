25a413801d1aab0329c8b4fc983e044a04445c21
george <gkephart@cnm.edu>
2019-03-31 13:36:57 -0600

    63 displaying custom content on the dom in a child component.

--- class-based-components-4/src/SeasonDisplay.js
+++ class-based-components-4/src/SeasonDisplay.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
+const getSeason = (lat, month) => {
+	if( month > 2 && month < 9) {
+		return lat > 0 ? 'summer' : 'winter';
+	} else {}
+	return lat > 0 ? 'winter' : 'summer';
+
+};
+
 
 const SeasonDisplay = (props) => {
-	console.log(props);
-
+	const season = getSeason(props.lat, new Date().getMonth());
 	return (
-		<div>Mason's super soft birthday party</div>
+		<h2>Mason's super soft birthday party happens in the {season}</h2>
 	);
 };
 
