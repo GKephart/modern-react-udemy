8beb3cfed33ecbe62209285db91ca3734dea557d
george <gkephart@cnm.edu>
2019-03-31 13:05:58 -0600

    61 skipped 60 cleaned up constructor to take advantage of babel look at code for more info

--- class-based-components-4/src/index.js
+++ class-based-components-4/src/index.js
@@ -4,19 +4,15 @@ import ReactDOM from 'react-dom'
 
 class App extends React.Component {
 
+	// abbreviated syntax that takes advantage of babel to setup/define the constructor
+	state = {lat: null, foo: "poo", error: ''};
 
-
-	//same constructor different language
-	constructor(props) {
+	//equivalent to
+	/*constructor(props) {
 		//supper passes data into React.Component's constructor.
 		super(props);
-
-		// sets up state for the App object
-		this.state = {lat: null, foo: "poo"};
-
-
-
-	}
+		//this.state = {lat: null, foo: "poo", error: ''};
+	*/
 
 	/**
 	 * init loading of data should only occur in the componentDidMount metho
@@ -55,7 +51,7 @@ class App extends React.Component {
 
 		if(!this.state.error && this.state.lat) {
 			return <div>Mason {foo} bear is eating a jar of honey @ {this.state.lat} </div>
-		} else if (!this.state.error && !this.state.lat !== -35){
+		} else if (this.state.error && !this.state.lat !== -35){
 			return <div>Mason {foo} Bear caused {this.state.error}</div>
 		}
 		return <h1>Y U NO let me load</h1>
