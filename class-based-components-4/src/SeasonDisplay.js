3e1c96afa671f423dde37fdb62a548a9ea4c1474
george <gkephart@cnm.edu>
2019-03-31 14:17:52 -0600

    65 more clean up

--- class-based-components-4/src/SeasonDisplay.js
+++ class-based-components-4/src/SeasonDisplay.js
@@ -1,16 +1,33 @@
 import React from 'react';
 
+const seasonConfig = {
+	summer: {
+		text: "Mason's super soft party will be hawt",
+		iconName: 'fire'
+	},
+	winter: {
+		text: "Mason's super soft party will be chilly",
+		iconName: "ice"
+
+	}
+};
+
 const getSeason = (lat, month) => {
 	let season = month > 2 && month < 9 ?
-			lat > 0 ? 'summer' : 'winter' :
-			lat > 0 ? 'winter' : 'summer';
+		lat > 0 ? 'summer' : 'winter' :
+		lat > 0 ? 'winter' : 'summer';
 	return season
 };
 const SeasonDisplay = (props) => {
-	const season = getSeason(props.lat, new Date().getMonth());
-	//  happens in the {season}
+	const currentSeason = getSeason(props.lat, new Date().getMonth());
+	const {text, icon} = seasonConfig[currentSeason];
+
 	return (
-		<h2>{season === 'winter' ? "Mason's super soft party will be chilly" : "Mason's super soft party will be hawt"}</h2>
+		<>
+			<i className={`${icon} icon`}/>
+			<h2>{text}</h2>
+			<i className={`${icon} icon`}/>
+		</>
 	);
 };
 export default SeasonDisplay;
