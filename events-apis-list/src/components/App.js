bb8ff05ec52466641a2ec7c9913b97880d97e5fb
george <gkephart@cnm.edu>
2019-03-31 22:29:11 -0600

    88 ran into a stupid error calling it a night

--- events-apis-list/src/components/App.js
+++ events-apis-list/src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
 	onSearchSubmit = (term) => {
-		console.log(term);
+		axios.get("http://api.unsplash.com/search/photos/?client_id=39e127fbd342d1221079182f004746c9933d29157af9bbeb6484bd9e82e72786" + "&query=" + term)
 	};
 
 	render() {
