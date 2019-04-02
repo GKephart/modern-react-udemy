4f59bab57e36ffbee6baca21b7f81ad1d2036510
george <gkephart@cnm.edu>
2019-04-01 20:49:58 -0600

    91 learned about async await I really like it.

--- events-apis-list/src/components/App.js
+++ events-apis-list/src/components/App.js
@@ -3,13 +3,14 @@ import axios from 'axios';
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-	onSearchSubmit = (term) => {
-		axios.get("https://api.unsplash.com/search/photos", {
+	 onSearchSubmit = async (term) => {
+	const reply =	await axios.get("https://api.unsplash.com/search/photos", {
 			params: {query: term},
 			headers: {
 				Authorization: "Client-ID 39e127fbd342d1221079182f004746c9933d29157af9bbeb6484bd9e82e72786"
 			}
-		})
+		});
+		 console.log(reply.data.results)
 	};
 
 	render() {
