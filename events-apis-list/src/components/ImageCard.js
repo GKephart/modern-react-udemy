fcec1c70a7ee19a1a9031233fc3599c7b6c1a302
george <gkephart@cnm.edu>
2019-04-03 18:33:16 -0600

    106 finished react with events lists and apis. Moving onto 128 reduct

--- events-apis-list/src/components/ImageCard.js
+++ events-apis-list/src/components/ImageCard.js
@@ -5,22 +5,26 @@ class ImageCard extends React.Component {
 	constructor(props) {
 		super(props);
 
+		this.state = {span: 0};
 		this.imageRef = React.createRef();
 	}
 
 	componentDidMount() {
-		this.imageRef.current.addEventListener("load", this.setSpan)
+		this.imageRef.current.addEventListener("load", this.setSpan())
 	}
 
-   setSpan = () => {
-		console.log(this.imageRef.current.clientHeight);
+	setSpan = () => {
+		const height = this.imageRef.current.clientHeight;
+		const span = Math.ceil(height /10);
+		this.setState({span});
 	};
 
 	render() {
 		let {urls, description} = this.props.image;
-		return(
-			<div>
+		return (
+			<div style={{gridRowEnd: `span ${this.state.span}`}} >
 				<img
+
 					ref={this.imageRef}
 					src={urls.regular}
 					alt={description}
