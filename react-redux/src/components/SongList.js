3fbaa02acee45d40b0df66e1972e77416fe17ab8
george <gkephart@cnm.edu>
2019-04-04 00:52:09 -0600

    148 got the list of songs to display

--- react-redux/src/components/SongList.js
+++ react-redux/src/components/SongList.js
@@ -2,14 +2,30 @@ import React, {Component} from "react";
 import {connect} from "react-redux";
 
 class SongList extends Component {
+
+	renderList= () => (
+		this.props.songs.map(song => (
+			<div className="item" key={song.title}>
+				<div className="right floated content">
+					<div className="ui button primary">Select</div>
+				</div>
+				<div className="content">
+					{song.title}
+				</div>
+			</div>
+			)
+		)
+	);
 	render() {
-		return <div>song list </div>
+
+		return (
+
+			<div className="ui divided list">{this.renderList()}</div>
+		)
 	}
 }
 
 const mapStateToProps = state => {
-	console.log(state);
-
 	return state;
 
 };
