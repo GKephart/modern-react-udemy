b3c01f8ac034f38c51d0a67e1847fa719e1ceeed
george <gkephart@cnm.edu>
2019-04-08 20:32:29 -0600

    158 more boilerplate added a new diagram

--- redux-thunk/src/components/App.js
+++ redux-thunk/src/components/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import PostList from "./PostList";
 
 const App = () => {
-	return <div className="ui container">react</div>
+	return (<div className="ui container"><PostList /></div>);
 };
 
 export default App;
