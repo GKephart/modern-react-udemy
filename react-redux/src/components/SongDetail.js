5da140b4706b5fbdee2502ea4464c52872da549f
george <gkephart@cnm.edu>
2019-04-08 18:47:00 -0600

    152 finished redux

--- react-redux/src/components/SongDetail.js
+++ react-redux/src/components/SongDetail.js
@@ -2,14 +2,34 @@ import React from "react";
 import {connect} from "react-redux";
 
 
+const SongDetail = (props) => {
+	console.log(props.song);
 
-const SongDetail = props => {
-	console.log(props);
-	return <div>Song Detail</div>;
+	if(props.song !== null) {
+		return (
+			<div className="ui card">
+				<div className="content">
+					<span className="header">{props.song.title}</span>
+					<div className="meta">
+						<span className="date">rocking since 1992</span>
+					</div>
+					<div className="description">
+						{props.song.duration}
+					</div>
+				</div>
+				<div className="extra content">
+					<i className="music icon"></i>
+				</div>
+			</div>
+		);
+	}
+	return <div>fuck off</div>
+
+
 };
 
 const mapStateToProps = (state) => {
-	return {song: state.selectSong };
+	return {song: state.selectedSong};
 
 };
 
