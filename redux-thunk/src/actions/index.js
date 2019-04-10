a7972a83ffb62b59cb6b0c4539cca8b7ff94210b
george <gkephart@cnm.edu>
2019-04-10 15:36:24 -0600

    176 got the posts to show. had to go back and change the action to return just the data not everything else

--- redux-thunk/src/actions/index.js
+++ redux-thunk/src/actions/index.js
@@ -3,5 +3,5 @@ import jsonPlaceHolder from "../api/jsonPlaceHolder"
 
 export const fetchPosts = () => async dispatch => {
 	const payload =  await jsonPlaceHolder.get("posts");
-	dispatch({type: "FETCH_POSTS",payload : payload });
+	dispatch({type: "FETCH_POSTS",payload : payload.data });
 };
--- redux-thunk/src/components/PostList.js
+++ redux-thunk/src/components/PostList.js
@@ -3,15 +3,33 @@ import {connect} from "react-redux";
 import {fetchPosts} from "../actions";
 
 class PostList extends Component {
-	 componentDidMount() {
-	 	this.props.fetchPosts();
-	 };
+	componentDidMount() {
+		this.props.fetchPosts();
+	};
+
+	renderList() {
+		return (this.props.posts.map(post => {
+			return (
+				<div className="item" key={post.id}>
+					<i className="large middle aligned icon user"/>
+					<div className="content">
+						<div className="description">
+							{post.title}
+							{post.body}
+						</div>
+					</div>
+
+				</div>
+			)
+
+		}))
+	}
 
 
 	render() {
-		console.log(this.props.posts);
+		console.log(this.props);
 		return (
-			<h1>Post List</h1>
+			<div className="ui relaxed divided list">{this.renderList()}</div>
 		);
 	}
 }
@@ -21,5 +39,5 @@ const mapStateToProps = (state) => {
 
 };
 
-export default connect(mapStateToProps, { fetchPosts }
+export default connect(mapStateToProps, {fetchPosts}
 )(PostList);
