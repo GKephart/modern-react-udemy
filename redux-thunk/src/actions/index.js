fd9824a36910e5c01e2cbae280e52d8c1030a2ab
george <gkephart@cnm.edu>
2019-04-11 16:29:25 -0600

    178 created the fetchUser action creator

--- redux-thunk/src/actions/index.js
+++ redux-thunk/src/actions/index.js
@@ -5,3 +5,8 @@ export const fetchPosts = () => async dispatch => {
 	const payload =  await jsonPlaceHolder.get("posts");
 	dispatch({type: "FETCH_POSTS",payload : payload.data });
 };
+
+export const fetchUser = (id) => async dispatch => {
+	const response = await  jsonPlaceHolder.get(`users/${id}`);
+	dispatch({type: "FETCH_USER", payload: response});
+};
